8276575e75bc69bc386feb2000a167bfc7bfad69
Kayla <kaygarrett18@yahoo.com>
2020-07-29 16:30:11 -0700

    Initial Commit

---
+++ CopngCovid2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CopngCovid2
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
